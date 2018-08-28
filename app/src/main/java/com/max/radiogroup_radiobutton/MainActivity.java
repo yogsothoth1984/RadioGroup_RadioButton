dff705cc689b67a6312f70765b58c767333f5b8e
max <40814622+vloxomire@users.noreply.github.com>
2018-08-28 09:17:49 -0300

    0917

--- app/src/main/java/com/max/radiogroup_radiobutton/MainActivity.java
+++ app/src/main/java/com/max/radiogroup_radiobutton/MainActivity.java
@@ -2,12 +2,25 @@ package com.max.radiogroup_radiobutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private EditText et1, et2;
+private Button botOperar;
+private TextView tv1;
+private RadioButton rbSuma, rbResta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et1=findViewById(R.id.et1);
+        et2=findViewById(R.id.et2);
+        botOperar=findViewById(R.id.botOperar);
+        tv1=findViewById(R.id.tv1);
+        rbSuma=findViewById(R.id.rbSuma);
+        rbResta=findViewById(R.id.rbResta);
     }
 }
